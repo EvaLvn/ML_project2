a78f6cdc6260a701ad5db274c627e7c6a66bdd54
Mathiponds <mathieu.desponds@epfl.ch>
2022-12-18 17:37:41 +0100

    Bert and LDA cleaned

--- Algorithms/bert_helpers.py
+++ Algorithms/bert_helpers.py
@@ -1,5 +1,8 @@
 import pickle
 import pandas as pd
+
+import gensim.corpora as corpora
+from gensim.models.coherencemodel import CoherenceModel
 from nltk.corpus import stopwords
 from sentence_transformers import SentenceTransformer
 
@@ -7,6 +10,7 @@ from bertopic import BERTopic
 from sklearn.feature_extraction.text import CountVectorizer
 from hdbscan import HDBSCAN
 from umap import UMAP
+
 
 def save_model(model, params, message = ""):
     saved_models.append((model, params, message))
@@ -51,3 +55,31 @@ def load_data_bert(country):
 
 def get_tweets_of_topic(topics, topic_nb, tweets, n):
     return [tweet for i, tweet in enumerate(tweets) if topics[i] == topic_nb][:n]
+
+def get_coherence(model, tweets, topics, coherence = 'c_v'):    # Preprocess Documents
+    documents = pd.DataFrame({"Document": tweets,
+                              "ID": range(len(tweets)),
+                              "Topic": topics})
+    documents_per_topic = documents.groupby(['Topic'], as_index=False).agg({'Document': ' '.join})
+    cleaned_docs = model._preprocess_text(documents_per_topic.Document.values)
+
+    # Extract vectorizer and analyzer from BERTopic
+    vectorizer = model.vectorizer_model
+    analyzer = vectorizer.build_analyzer()
+
+    # Extract features for Topic Coherence evaluation
+    words = vectorizer.get_feature_names()
+    tokens = [analyzer(doc) for doc in cleaned_docs]
+    dictionary = corpora.Dictionary(tokens)
+    corpus = [dictionary.doc2bow(token) for token in tokens]
+    topic_words = [[words for words, _ in model.get_topic(topic)] 
+                   for topic in range(len(set(topics))-1)]
+    # Evaluate
+    coherence_model = CoherenceModel(topics=topic_words, 
+                                     texts=tokens, 
+                                     corpus=corpus,
+                                     dictionary=dictionary, 
+                                     coherence=coherence)
+    coherence = coherence_model.get_coherence()
+
+    return coherence
